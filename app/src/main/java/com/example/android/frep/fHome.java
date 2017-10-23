c0c123ee6c01edd2a9efa0b374a61f98196c92f0
Daniel Go <danielgo13@users.noreply.github.com>
2017-10-23 14:57:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/frep/fHome.java
@@ -0,0 +1,13 @@
+package com.example.android.frep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_home);
+    }
+}
